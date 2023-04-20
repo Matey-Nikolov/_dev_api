5f7e411c8ff9ea16459d498b52d1da1ad7e88f70
Matey-Nikolov <matey.nikolov@mail.bg>
2023-04-20 20:06:38 +0300

    edit register css

--- src/js-lit/registerLit.js
+++ src/js-lit/registerLit.js
@@ -1,6 +1,65 @@
 import { html } from '../Global/globalLit.js';
 
 const registerTemplate = () =>{
+return html`
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-7">
+                            <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                <div class="card-header"><h3 class="text-center font-weight-light my-4">Create Account</h3></div>
+                                <div class="card-body">
+                                    <form>
+                                        <div class="row mb-3">
+                                            <div class="col-md-6">
+                                                <div class="form-floating mb-3 mb-md-0">
+                                                    <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" />
+                                                    <label for="inputFirstName">First name</label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-floating">
+                                                    <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" />
+                                                    <label for="inputLastName">Last name</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <input id="role" class="form-control" type="text" placeholder="guest or admin" />
+                                            <label for="role">Role</label>
+                                        </div>
+                                        <div class="row mb-3">
+                                            <div class="col-md-6">
+                                                <div class="form-floating mb-3 mb-md-0">
+                                                    <input class="form-control" id="inputPassword" type="password" placeholder="Create a password" />
+                                                    <label for="inputPassword">client_id</label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-floating mb-3 mb-md-0">
+                                                    <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="Confirm password" />
+                                                    <label for="inputPasswordConfirm">Confirm Password</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="mt-4 mb-0">
+                                            <div class="d-grid"><a class="btn btn-primary btn-block" href="login.html">Create Account</a></div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    `;
+};
+
+/*
+const registerTemplate = () =>{
     return html`
     <div class="col-md-4 p-5 shadow-sm border rounded-5 border-primary">
         <h2 class="text-center mb-4 text-primary">Register</h2>
@@ -24,5 +83,5 @@ const registerTemplate = () =>{
         </form>
     </div>`;
 };
-
+*/
 export { registerTemplate };
