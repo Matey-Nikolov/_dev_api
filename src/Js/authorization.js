55e0552063aacd42c725f77f8dcb2c57429badcc
Matey <matey.nikolov@mail.bg>
2023-05-29 18:57:29 +0300

    no local storage in project except refreshToken.js

--- src/Js/authorization.js
+++ src/Js/authorization.js
@@ -1,10 +1,10 @@
 import { authorization } from './global.js'
 import { render, welcomePage, afterAuthorization } from '../Global/globalLit.js';
-import { divApp } from '../controller/homeController.js';
+import { divApp, newAccess_token } from '../Global/globalInport.js';
 
 async function whoIam(){
   let myHeaders = new Headers();
-  myHeaders.append('Authorization', 'Bearer ' + sessionStorage.getItem('token'));
+  myHeaders.append('Authorization', 'Bearer ' + newAccess_token);
 
   const requestOptions = {
     method: 'GET',
@@ -12,14 +12,14 @@ async function whoIam(){
     redirect: 'follow'
   };
 
-    await fetch(`https://api.central.sophos.com/whoami/v1`, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-      
-        authorization(result.id, result['apiHosts'].dataRegion);
-        render(welcomePage(afterAuthorization('You are authorized! You are authorized. This takes about an hour. Therefore, you are required to renew it every hour.')), divApp);
-    })
-    .catch(error => console.log('error', error));
+  await fetch(`https://api.central.sophos.com/whoami/v1`, requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    
+      authorization(result.id, result['apiHosts'].dataRegion, newAccess_token);
+      render(welcomePage(afterAuthorization('You are authorized! You are authorized. This takes about an hour. Therefore, you are required to renew it every hour.')), divApp);
+  })
+  .catch(error => console.log('error', error));
 };
 
 
