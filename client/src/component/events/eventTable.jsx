57aeeaffe4deb387dd27d204c4a67464188c78b3
Matey <matey.nikolov@mail.bg>
2024-01-16 13:49:49 +0200

    Add event component

---
+++ client/src/component/events/eventTable.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
+
+import { useGlobalState } from '../../hooks';
+
+import { fetchEvents } from '../../Services/eventsService';
+
+const EventTable = () => {
+  const [tenetId] = useGlobalState('tenetId');
+  const [tokenTenat] = useGlobalState('tokenTenat');
+
+  const filterRegex = /Event::([A-Za-z]+)::([A-Za-z]+)/;
+
+  const [useDataGetEvents] = useState({ tenetId, tokenTenat });
+
+  const [useEvents, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const eventsData = await fetchEvents(useDataGetEvents);
+
+        setEvents(eventsData.items);
+      } catch (error) {
+        console.error('Error fetching events:', error);
+        throw error;
+      }
+    };
+
+    fetchData();
+  }, []);
+  
+  return(
+    <Table responsive bordered striped className="mt-2">
+      <thead>
+        <tr>
+          <th scope="col">Messages</th>
+          <th scope="col">Allow</th>
+        </tr>
+      </thead>
+      <tbody>
+        {useEvents.map((value) => (
+          <tr key={value.name}>
+            <td>{value.name}</td>
+            <td>
+              {filterRegex.test(value.type) && filterRegex.exec(value.type) [2] === 'WebControlViolation' ? (
+                <button
+                  data-type={value.name}
+                  className="btn btn-outline-success"
+                >
+                  allow
+                </button>
+              ) : (
+                null
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default EventTable;
