64c0893fb5dad3ce8658944c583256c35e175ccf
Matey <matey.nikolov@mail.bg>
2024-01-20 18:04:29 +0200

    Server
    Update website and add block for website

--- server/Router/websiteRouter.js
+++ server/Router/websiteRouter.js
@@ -43,5 +43,42 @@ router.get(
     }
 );
 
+router.get(
+    '/delete',
+    [
+        query('accessToken').isLength({ min: 1 }).trim().escape(),
+        query('access_Id').isLength({ min: 1 }).trim().escape(),
+        query('website_Id').isLength({ min: 1 }).trim().escape(),
+    ],
+    async (req, res) => {
+        
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        
+        const { accessToken, access_Id, website_Id } = req.query;
+
+        const axiosConfig = {
+            headers: {
+              'X-Tenant-ID': access_Id,
+              'Authorization': `Bearer ${accessToken}`
+            }
+        };
+
+        try{
+            // https://api-eu01.central.sophos.com
+            const response = await axios.delete(`https://api-eu01.central.sophos.com/endpoint/v1/settings/web-control/local-sites/${website_Id}`, axiosConfig);
+            
+            res.json(response.data.deleted);
+        }
+        catch(error){
+            console.error('Error posting data to external URL:', error.message);
+  
+            res.status(500).json({ success: false, message: 'Error posting data to external URL' });
+        };
+    }
+);
 
 export default router;
