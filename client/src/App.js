5e76e7a2b596d4b750e6f273bcf952c50f41f264
Matey <matey.nikolov@mail.bg>
2024-01-23 11:33:49 +0200

    Update
    Use lazy and Suspense - experimental

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,42 @@
-import React, { useState } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Navbar from './component/navbar/Navbar';
-
 import Footer from './component/Footer/Footer';
-import HomePage from './component/HomePage';
-import LoginPage from './component/auth/Login';
-import RegisterPage from './component/register';
-import AlertTable from './component/alert/alert';
 import useToken from './Services/useToken';
-import EndpointPage from './component/endpoint/endpointPage';
-import EventTable from './component/events/eventTable';
-import WebsiteTable from './component/website/websiteTable';
+
+const HomePage = lazy(() => import('./component/HomePage'));
+const LoginPage = lazy(() => import('./component/auth/Login'));
+// const RegisterPage = lazy(() => import('./component/register'));
+const AlertPage = lazy(() => import('./component/alert/Alert'));
+const EndpointPage = lazy(() => import('./component/endpoint/endpointPage'));
+const EventTable = lazy(() => import('./component/events/eventTable'));
+const WebsiteTable = lazy(() => import('./component/website/websiteTable'));
 
 function App() {
   const { token, setToken } = useToken();
 
-  if(!token) {
-    return <LoginPage setToken={setToken} />
-  };
+  if (!token) {
+    return <LoginPage setToken={setToken} />;
+  }
 
   return (
-    <Router> 
+    <Router>
       <div className="App">
         <Navbar />
         <div className="container-fluid">
           <div className="row">
-            <div className="col-md-10">
-              <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/register' element={<RegisterPage />} />
-                <Route path='/alerts' element={<AlertTable />} />
-                <Route path='/endpoints' element={<EndpointPage />} />
-                <Route path='/events' element={<EventTable />} />
-                <Route path='/websites' element={<WebsiteTable />} />
-              </Routes>
+            <div>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  {/* <Route path='/register' element={<RegisterPage />} /> */}
+                  <Route path="/alerts" element={<AlertPage />} />
+                  <Route path="/endpoints" element={<EndpointPage />} />
+                  <Route path="/events" element={<EventTable />} />
+                  <Route path="/websites" element={<WebsiteTable />} />
+                </Routes>
+              </Suspense>
             </div>
           </div>
         </div>
