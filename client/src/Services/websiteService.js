988bd1b7d46da1a7d93c8ad57e5f0fec787f9886
Matey <matey.nikolov@mail.bg>
2024-01-25 19:50:45 +0200

    Add
    Add server path

--- client/src/Services/websiteService.js
+++ client/src/Services/websiteService.js
@@ -3,96 +3,106 @@ import ApiWebsite from '../axiosrequests/apiWebsite.js';
 const apiRequestWebsite = new ApiWebsite();
 
 class WebsiteService {
-    constructor(accessToken, id) {
-      this.regexWebsite = /(?:https?:\/\/www\.)?(?<hostname>[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b)([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/;
-      this.accessToken = accessToken;
-      this.id = id;
-      this.isDeleted = false;
+  constructor(accessToken, id) {
+    this.regexWebsite = /(?:https?:\/\/www\.)?(?<hostname>[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b)([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/;
+    
+    this.accessToken = accessToken;
+    this.id = id;
+    
+    this.setWebsiteInformation = new Set();
+    this.setWebsiteURL = new Set();
+
+    this.isDeleted = false;
+
+    this.isAddWebsite = {
+      'status': -1,
+      'information': { }
+    };
+  }
+
+  getWebsiteData = async () => {
+    //apiRequestWebsite.setCredentials();
+
+    const data = await apiRequestWebsite.getWebsite(this.accessToken, this.id);
+
+    return data.items;
+  };
+
+  allowWebsite = async (typeName) => {
+    const websiteData = await this.getWebsiteData();
+
+    switch(typeName){
+      case 'allow':
+        websiteData.map((value) => {
+          this.setWebsiteInformation.add(value.url);
+        });
+      break
+      default:
+        websiteData.map((value) => {
+          let data = 
+          { 
+            id: value.id,
+            url: value.url,
+            comment: value.comment   
+          };
+
+          this.setWebsiteInformation.add(data);
+          this.setWebsiteURL.add(value.url);
+        });      
+      break;
+    };
+    
+    return this.setWebsiteInformation;
+  };
+
+  // addAllowedWebsite = async (valueURL) => {
+  //   const setWebsite = await this.allowWebsite('allow');
+  //   const match = this.regexWebsite.exec(valueURL);
+  //   const urlExtract = match ? match[1] : null;
+
+  //   if (setWebsite.has(urlExtract)) {
+  //     return true;
+  //   } else if (urlExtract !== null) {
+  //     try {
+  //       console.log('WORK');
+  //       this.getWebsiteAllow(urlExtract);
+  //       await ApiWebsite.postRequest('/data/websites/post');
+  //       return false;
+  //     } catch (error) {
+  //       console.error('Error adding allowed website:', error);
+  //       return null;
+  //     }
+  //   }
+
+  //   return null;
+  // };
+
+  btnBlockWebsite = async (website_Id) => {
+    try {
+      this.isDeleted = await apiRequestWebsite.deleteRequest(this.accessToken, this.id, website_Id);
+    } catch (error) {
+      console.error('Error blocking website:', error);
+    };
+
+    return this.isDeleted.data;
+  };
+
+  btnAllowWebsite = async (valueURL) => {
+    const extractedURL = (this.regexWebsite.exec(valueURL) || [])[1];
+
+    console.log(this.setWebsiteURL);
+
+    if (!this.setWebsiteURL.has(extractedURL)) {
+      this.isAddWebsite = await apiRequestWebsite.addWebsiteRequest(this.accessToken, this.id, extractedURL);
     }
 
-    getWebsiteData = async () => {
-      //apiRequestWebsite.setCredentials();
-
-      const data = await apiRequestWebsite.getWebsite(this.accessToken, this.id);
-
-      return data.items;
+    if (this.isAddWebsite.status === 200) {
+      this.setWebsiteURL.add(this.isAddWebsite.information.url);
+      this.setWebsiteInformation.add(this.isAddWebsite.information);
     };
-  
-    allowWebsite = async (typeName) => {
-      const websiteData = await this.getWebsiteData();
-      const setWebsite = new Set();
-  
-      switch(typeName){
-        case 'allow':
-          websiteData.map((value) => {
-            setWebsite.add(value.url);
-          });
-        break
-        default:
-          websiteData.map((value) => {
-            let data = 
-            { 
-              id: value.id,
-              url: value.url,
-              comment: value.comment   
-            };
-  
-            setWebsite.add(data);
-          });      
-        break;
-      };
 
-      console.log(setWebsite);
-  
-      return setWebsite;
-    };
-  
-    // addAllowedWebsite = async (valueURL) => {
-    //   const setWebsite = await this.allowWebsite('allow');
-    //   const match = this.regexWebsite.exec(valueURL);
-    //   const urlExtract = match ? match[1] : null;
-  
-    //   if (setWebsite.has(urlExtract)) {
-    //     return true;
-    //   } else if (urlExtract !== null) {
-    //     try {
-    //       console.log('WORK');
-    //       this.getWebsiteAllow(urlExtract);
-    //       await ApiWebsite.postRequest('/data/websites/post');
-    //       return false;
-    //     } catch (error) {
-    //       console.error('Error adding allowed website:', error);
-    //       return null;
-    //     }
-    //   }
-  
-    //   return null;
-    // };
-  
-    btnBlockWebsite = async (website_Id) => {
-      try {
-        this.isDeleted = await apiRequestWebsite.deleteRequest(this.accessToken, this.id, website_Id);
-      } catch (error) {
-        console.error('Error blocking website:', error);
-      };
-
-      return this.isDeleted.data;
-    };
-  
-    // btnAllowWebsite = async (valueURL) => {
-    //   const extractedURL = (this.regexWebsite.exec(valueURL) || [])[1];
-    //   this.getWebsiteAllow(extractedURL);
-  
-    //   try {
-    //     await ApiWebsite.postRequest('/data/websites/post');
-    //   } catch (error) {
-    //     console.error('Error allowing website:', error);
-    //   }
-    // };
-  
-    // getWebsiteAllow = (urlExtract) => {
-    //   // Implement your logic for getting allowed websites here
-    // };
+    return this.isAddWebsite.status;
+  };
 };
   
 export default WebsiteService;
